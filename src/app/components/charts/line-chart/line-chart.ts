72b8b9c724f37ac137a25479cc384a015510e258
NizoDotNet <mmmdovnizami.official@gmail.com>
2026-03-21 14:48:55 +0400

    curved line. need work on logic which will come from back

--- src/app/components/charts/line-chart/line-chart.ts
+++ src/app/components/charts/line-chart/line-chart.ts
@@ -9,21 +9,34 @@ import { BaseChartDirective } from 'ng2-charts';
   styleUrl: './line-chart.css',
 })
 export class LineChart {
-  incomeData = input<any[]>([]);
-  expenseData = input<any[]>([]);
+  incomeData = input<any[]>([10, 20, 30, 20, 2, 12, 30]);
+  expenseData = input<any[]>([4, 12, 32]);
   public lineChartData: ChartConfiguration['data'] = {
     datasets: [
       {
         label: 'Income',
         borderColor: 'rgb(0, 184, 148)',
         data: this.incomeData(),
+        tension: 0.4,
       },
       {
         label: 'Expense',
         borderColor: 'rgb(239, 68, 68)',
         data: this.expenseData(),
+        tension: 0.4,
       },
     ],
     labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'],
+  };
+
+  lineChartOptions: ChartConfiguration['options'] = {
+    plugins: {
+      legend: {
+        labels: {
+          boxHeight: 1,
+        },
+        position: 'bottom',
+      },
+    },
   };
 }
