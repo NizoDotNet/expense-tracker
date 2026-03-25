f9318a32d5132d655e776dfa4ad9ab4b425caf1d
NizoDotNet <mmmdovnizami.official@gmail.com>
2026-03-25 13:23:15 +0400

    need work on services

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,12 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/auth/login/login';
 export const routes: Routes = [
   {
     path: 'dashboard',
     loadComponent: () => import('./pages/dashboard/dashboard').then((c) => c.Dashboard),
+  },
+  {
+    path: 'auth/login',
+    component: Login,
   },
 ];
