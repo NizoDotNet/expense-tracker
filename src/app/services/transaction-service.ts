bd2f396757f2e714a9f40ad7b4a62567aaf2249a
NizoDotNet <mmmdovnizami.official@gmail.com>
2026-03-27 20:29:51 +0400

    need test all

--- src/app/services/transaction-service.ts
+++ src/app/services/transaction-service.ts
@@ -1,6 +1,50 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
 
+export enum TimePeriod {
+  day = 0,
+  month = 1,
+}
 @Injectable({
   providedIn: 'root',
 })
-export class TransactionService {}
+export class TransactionService {
+  readonly http = inject(HttpClient);
+  readonly baseUrl = '/api/transactions';
+
+  getAllTransactions(page: number, pageNumber: number) {
+    return this.http.get(`${this.baseUrl}`, {
+      withCredentials: true,
+      params: {
+        page,
+        pageNumber,
+      },
+    });
+  }
+
+  deleteTransaction(transactionId: string) {
+    return this.http.delete(`${this.baseUrl}/${transactionId}`. {
+      withCredentials: true
+    });
+  }
+
+  getByCategory(timePeriod: TimePeriod, date: string) {
+    return this.http.get(`${this.baseUrl}/by-category`, {
+      withCredentials: true,
+      params: {
+        timePeriod,
+        date,
+      },
+    });
+  }
+
+  getIncomeExpense(timePeriod: TimePeriod, dateTime: string) {
+    return this.http.get(`${this.baseUrl}/income-expense`, {
+      withCredentials: true,
+      params: {
+        timePeriod,
+        dateTime,
+      },
+    });
+  }
+}
