81e7d8a90da0cdd09abdba340186893dd9493a54
NizoDotNet <mmmdovnizami.official@gmail.com>
2026-03-29 01:36:31 +0400

    integration worked

--- src/app/pages/dashboard/components/table/table.ts
+++ src/app/pages/dashboard/components/table/table.ts
@@ -1,9 +1,10 @@
 import { Component, input } from '@angular/core';
 import { TransactionResponse } from '../../../../services/transaction-service';
 import { DatePipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-table',
-  imports: [DatePipe],
+  imports: [DatePipe, RouterLink],
   templateUrl: './table.html',
   styleUrl: './table.css',
 })
--- src/app/pages/transactions/transactions.ts
+++ src/app/pages/transactions/transactions.ts
@@ -1,5 +1,9 @@
-import { Component, input } from '@angular/core';
-import { PagedResult, TransactionResponse } from '../../services/transaction-service';
+import { Component, inject, input, OnInit, signal } from '@angular/core';
+import {
+  PagedResult,
+  TransactionResponse,
+  TransactionService,
+} from '../../services/transaction-service';
 
 @Component({
   selector: 'app-transactions',
@@ -7,8 +11,19 @@ import { PagedResult, TransactionResponse } from '../../services/transaction-ser
   templateUrl: './transactions.html',
   styleUrl: './transactions.css',
 })
-export class Transactions {
-  transactions = input<PagedResult<TransactionResponse>>();
+export class Transactions implements OnInit {
+  readonly transactionService = inject(TransactionService);
+  page = signal<number>(1);
+  pageSize = signal<number>(10);
+
+  transactions = signal<PagedResult<TransactionResponse> | null>(null);
+  ngOnInit(): void {
+    this.transactionService.getAllTransactions(1, 10).subscribe({
+      next: (value) => {
+        this.transactions.set(value);
+      },
+    });
+  }
 
   Abs(value: number) {
     return value >= 0 ? value : -value;
