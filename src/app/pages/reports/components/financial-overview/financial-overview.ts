b8fb41548d19de01880bbe54e5cb30b61e6e1ae4
NizoDotNet <mmmdovnizami.official@gmail.com>
2026-04-03 00:13:09 +0400

    working on reports page

---
+++ src/app/pages/reports/components/financial-overview/financial-overview.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-financial-overview',
+  imports: [],
+  templateUrl: './financial-overview.html',
+  styleUrl: './financial-overview.css',
+})
+export class FinancialOverview {}
---
+++ src/app/pages/reports/reports.ts
@@ -0,0 +1,44 @@
+import { Component, inject, OnInit, signal } from '@angular/core';
+import {
+  TimePeriod,
+  TransactionExpenseByCategoryResponse,
+  TransactionIncomeExpenseResponse,
+  TransactionResponse,
+  TransactionService,
+} from '../../services/transaction-service';
+
+@Component({
+  selector: 'app-reports',
+  imports: [],
+  templateUrl: './reports.html',
+  styleUrl: './reports.css',
+})
+export class Reports implements OnInit {
+  transactionService = inject(TransactionService);
+  incomeExpense = signal<TransactionIncomeExpenseResponse | null>(null);
+  byCategory = signal<TransactionExpenseByCategoryResponse[] | null>(null);
+  timePeriod = signal<TimePeriod>(TimePeriod.month);
+  transactions = signal<TransactionResponse[]>([]);
+  date = signal<string>(new Date().toUTCString());
+
+  ngOnInit(): void {
+    this.getIncomeExpense();
+    this.getExpensesByCategory();
+  }
+
+  private getExpensesByCategory() {
+    this.transactionService.getByCategory(this.timePeriod(), this.date()).subscribe({
+      next: (value) => {
+        this.byCategory.set(value);
+      },
+    });
+  }
+
+  private getIncomeExpense() {
+    this.transactionService.getIncomeExpense(this.timePeriod(), this.date()).subscribe({
+      next: (value) => {
+        this.incomeExpense.set(value);
+      },
+    });
+  }
+}
