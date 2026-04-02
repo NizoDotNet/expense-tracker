15379520bb32166203f88a6a511994ea1d3eacaf
NizoDotNet <mmmdovnizami.official@gmail.com>
2026-04-03 00:38:55 +0400

    need test and add bar chart

--- src/app/pages/reports/components/financial-overview/financial-overview.ts
+++ src/app/pages/reports/components/financial-overview/financial-overview.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
-
+import { Component, input } from '@angular/core';
+import {
+  TransactionExpenseByCategoryResponse,
+  TransactionIncomeExpenseResponse,
+} from '../../../../services/transaction-service';
+import { LineChart } from '../../../../components/charts/line-chart/line-chart';
+import { PieChart } from '../../../../components/charts/pie-chart/pie-chart';
 @Component({
   selector: 'app-financial-overview',
-  imports: [],
+  imports: [LineChart, PieChart],
   templateUrl: './financial-overview.html',
   styleUrl: './financial-overview.css',
 })
-export class FinancialOverview {}
+export class FinancialOverview {
+  incomeExpense = input.required<TransactionIncomeExpenseResponse>();
+  incomeByCategory = input.required<TransactionExpenseByCategoryResponse[]>();
+}
